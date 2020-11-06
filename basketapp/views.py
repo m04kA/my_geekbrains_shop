140f92098a8a13df89abcc34cb66ead6b9386fcc
Yaroslav <yarniyjack@gmail.com>
2020-11-06 05:42:16 +0300

    lesson_1_next_lv. URLs as regexp

--- basketapp/views.py
+++ basketapp/views.py
@@ -43,8 +43,13 @@ def basket_remove(request, pk):
 @login_required
 def basket_edit(request, pk, quantity):
     if request.is_ajax():
-        print(f"{pk} - {quantity}")
-        new_basket_item = Basket.objects.get(pk=int(pk))
+        try:
+            pk = int(pk)
+            quantity = int(quantity)
+        except Exception as exp:
+            print(f"Wrong input numbers! {exp}")
+            raise exp
+        new_basket_item = Basket.objects.get(pk=pk)
 
         if quantity > 0:
             new_basket_item.quantity = quantity
