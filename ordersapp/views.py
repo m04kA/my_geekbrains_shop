ede692d55e62ec1e8169b9191d0a55399f377342
Yaroslav <yarniyjack@gmail.com>
2021-01-14 10:05:46 +0300

    lesson_4_next_lv price and DB

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -120,8 +120,8 @@ def order_forming_complete(requesr, pk):
 
 @receiver(pre_save, sender=OrderItem)
 @receiver(pre_save, sender=Basket)
-def product_quantity_update_save(sender, update_filds, instance, **kwargs):
-    if update_filds is 'quantity' or 'product':
+def product_quantity_update_save(sender, update_fields, instance, **kwargs):
+    if update_fields is 'quantity' or 'product':
         if instance.pk:
             instance.product.quantity = instance.quantity - sender.get_item(instance.pk).quantity
         else:
