d90c975cbf66e697ebd77437d83f0c1b205c5ef7
Yaroslav <yarniyjack@gmail.com>
2021-01-27 15:59:12 +0300

    DataBase

--- best_shop/settings.py
+++ best_shop/settings.py
@@ -81,20 +81,20 @@ WSGI_APPLICATION = "best_shop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "best_shop",
-#         "USER": "postgres"
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "best_shop",
+        "USER": "postgres"
+    }
+}
 
 
 
